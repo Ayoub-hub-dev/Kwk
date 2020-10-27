6e5db1a51803dadb7819445d23b10a8241df29bf
AYOUB OUAHMAN <73245716+Ayoub-hub-dev@users.noreply.github.com>
2020-10-27 11:28:47 +0100

    first commit

---
+++ Guess_The_Wods.py
@@ -0,0 +1,9 @@
+# I will Create a web site of Guess The Word Contain two identical letters
+# Create a list for to add a new words to find
+# Add pointe for inctance if you find a word you get  one pointe
+def good_length():
+	inp = input("Enter your Password : ")
+	if len(inp) > 8 and len(inp) < 64 :
+		print("welecome")
+	print("your password is so long")
+good_length()		
